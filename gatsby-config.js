a05e097a82280509389fef44fc14a48d2b2b5d54
Pontus <pontus@psu.se>
2020-09-21 11:52:11 +0200

    edit config and gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,14 +6,37 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    // Used for the title template on pages other than the index site
     siteTitle: `Pontus Sundén`,
-    siteTitleAlt: `Pontus Sunden - psu.se`,
+    // Default title of the page
+    siteTitleAlt: `Pontus Sundén - psu.se`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Pontus Sundén`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://psu.se`,
+    // Used for SEO
+    siteDescription: `Personal blog and resumé for Pontus Sundén. A Communications professional with a passion for web development.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@pontus`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
+        basePath: `/`,
+        blogPath: `/blog`,
+        postsPath: `content/posts`,
+        mdx: true,
+        formatString: `YYYY-MM-DD`,
+        feed: true,
+        feedTitle: `Pontus Sundén - Blog`,
+        showLineNumbers: true,
+        showCopyButton: false,
         navigation: [
           {
             title: `Blog`,
